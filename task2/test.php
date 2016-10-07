34a3f92809c56c09126b2ce51da49fc9a7bb8919
odanylevskyi <aleksey@danilevsky.com>
2016-10-07 10:27:18 +0100

    Remove task description from test.php

--- task2/test.php
+++ task2/test.php
@@ -5,15 +5,6 @@
  * Date: 07/10/2016
  * Time: 09:25
  *
- * TASK:
- * We have some customer records in a text file (customers.json) -- one customer per line, JSON-encoded.
- * We want to invite any customer within 100km of our Dublin office for some food and drinks on us.
- * Write a program that will read the full list of customers and output the names and user ids of matching
- * customers (within 100km), sorted by User ID (ascending).
- * - You can use the first formula from this Wikipedia article to calculate distance.
- *   Don't forget, you'll need to convert degrees to radians.
- * - The GPS coordinates for our Dublin office are 53.3393,-6.2576841.
- * - You can find the Customer list here.
  */
 
 require_once("LatLng.php");
